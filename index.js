effbd6428c4f3a522ce1184bc28e063c84247036
shookethnasty <hchordiya6@gmail.com>
2025-03-01 21:56:21 +1100

    building some basic functionality

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 let firstCard = 10
-let secondCard = 4
-let sum = firstCard + secondCard
+let secondCard = 6
+let cards = [firstCard, secondCard]
+// let sum = cards[0] + cards[1]
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+
+// storing my cards in the cards id 
+let cardsEl = document.getElementById("cards-el")
+// storing my sum of the cards
+let sumEl = document.getElementById("sum-el")
+
 let messageEl = document.getElementById("message-el")
+// let cards = [firstCard, secondCard]
+let sum = cards[0] + cards[1]
 
 
 function startGame(){
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂"
+        message = "Do you want to draw a new card?"
+        cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+        // sum += cards[0] + cards[1]
+        sumEl.textContent = "Sum: " + sum
+
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳"
+        message = "Wohoo! You've got Blackjack!"
         hasBlackJack = true
     } else {
-        message = "You're out of the game! 😭"
+        message = "You're out of the game!"
         isAlive = false
     }
     
@@ -22,3 +35,8 @@ function startGame(){
     messageEl.textContent = message
 }
 
+function newCard(){
+    let newCard = 6
+    sum += newCard
+    sumEl.textContent = "Sum: " + sum
+}
